10932d8a273e6b1a50bf70e61e29a00f7e66ff81
roberto2002-03-12 <robertog-18@hotmail.com>
2024-05-11 13:09:52 -0500

    Ejemplos de: useValue, useFactory y ConfigService para envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,56 @@
 import { Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config'
+import { firstValueFrom } from 'rxjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { DatabaseModule } from './database/database.module';
+
+// const API_KEY = '12312412';
+// const API_KEY_PROD = 'PROD12312412';
+
+import { environments } from './environments';
+import config from './config';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+    }),
+    HttpModule,
+    UsersModule,
+    ProductsModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    // {
+    //   // nombre del provedor
+    //   provide: 'API_KEY',
+    //   // valor asignado
+    //   useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    // },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await firstValueFrom(
+          http.get('https://jsonplaceholder.typicode.com/todos')
+        );
+        return tasks.data;
+      },
+      inject: [HttpService],
+    },
+    AppService,
+  ],
 })
 export class AppModule {}
+/*
+useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY
+
+no te confundas, funciona el process.env.NODE_ENV directo en este caso porque
+se esta declarando al momento de correr la api.
+*/
